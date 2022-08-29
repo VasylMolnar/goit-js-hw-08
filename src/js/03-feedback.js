904d2f5587a151b299de5941a3390c2bd1bb9e64
VasylMolnar <vasylmolnar281@gmail.com>
2022-08-29 20:56:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,20 +18,7 @@ function onSubmitForm(e) {
   localStorage.removeItem('feedback-form-state');
 }
 
-/*
-(function dataFromLocalStorage() {
-   console.log(data);
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
-  }
-})();*/
-
 document.addEventListener('DOMContentLoaded', event => {
-  //console.log('DOM fully loaded and parseadsd');
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
