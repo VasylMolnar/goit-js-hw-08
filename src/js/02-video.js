329ad9049689c865b9fdaacfbc785c4f79b36d08
VasylMolnar <vasylmolnar281@gmail.com>
2022-08-29 13:28:33 +0300

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,16 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+//const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
   //console.log('played the video!');
+  //console.log(seconds);
 }
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
